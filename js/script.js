5d0bb1d72ddc623cfb4b4ae189c0a68e0918312c
MeetParmar777 <manpreetparmarmp@gmail.com>
2024-11-25 21:28:58 +0530

    -

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('.hero-banner-section').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      });
+
+
+
+
+
+});
